5386ad11e61871ba3b7f00c2cc9cd1916ba61d2f
Johan Nordström <johan@digitalvillage.se>
2020-04-06 11:24:31 +0200

    __dirname and __filename in node.

--- 01-intro/global.js
+++ 01-intro/global.js
@@ -8,10 +8,11 @@ console.log("Hello, node.js!");
 // 	console.log("I'll be back!");
 // }, 3000);
 
-let counter = 0;
-setInterval(function() {
-	counter = counter + 2;
-	console.log(`Hello after ${counter} seconds`);
-}, 2000);
+// let counter = 0;
+// setInterval(function() {
+// 	counter = counter + 2;
+// 	console.log(`Hello after ${counter} seconds`);
+// }, 2000);
 
-console.log("Bye now... (wave)");
+console.log("dirname in global.js is:", __dirname);
+console.log("filename in global.js is:", __filename);
