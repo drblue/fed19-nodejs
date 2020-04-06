63feece9cfef2a63b9b7009f8668cd2fcc7a4d41
Johan Nordström <johan@digitalvillage.se>
2020-04-06 13:43:07 +0200

    LOTS of fun with modules that uses modules!

--- 01-intro/calc.js
+++ 01-intro/calc.js
@@ -3,29 +3,17 @@
  */
 
 const circles = require('./modules/circles');
-const num = require('./modules/num');
-
-// console.log("Approximation of PI is:");
-// console.log(num.round(Math.PI, 5));
 
 let radius = 2;
-let area = circles.circleArea(radius);
+let area = circles.approxCircleArea(radius, 4);
 
-console.log(`The area of a circle with radius ${radius} is:`);
+console.log(`The approx. area of a circle with radius ${radius} is:`);
 console.log(area);
 console.log();
 
-console.log(`The approx. area of a circle with radius ${radius} is:`);
-console.log(num.round(area, 2));
-console.log();
-
 radius = 4;
-let circumference = circles.circleCircumference(radius);
-
-console.log(`The circumference of a circle with radius ${radius} is:`);
-console.log(circumference);
-console.log();
+let circumference = circles.approxCircleCircumference(radius, 2);
 
 console.log(`The approx. circumference of a circle with radius ${radius} is:`);
-console.log(num.round(circumference, 4));
+console.log(circumference);
 console.log();
--- 01-intro/modules/circles.js
+++ 01-intro/modules/circles.js
@@ -2,12 +2,18 @@
  * circles module
  */
 
+const num = require('./num');
 const { PI } = Math;
 
 const circleArea = (r) => r ** 2 * PI;
 const circleCircumference = r => 2 * PI * r;
 
+const approxCircleArea = (r, precision) => num.round(circleArea(r), precision);
+const approxCircleCircumference = (r, precision) => num.round(circleCircumference(r), precision);
+
 module.exports = {
 	circleArea,
 	circleCircumference,
+	approxCircleArea,
+	approxCircleCircumference,
 };
