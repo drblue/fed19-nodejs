ed4fae943c4494fc5a489f77d0ab14e271c9cd7a
Johan Nordström <johan@digitalvillage.se>
2020-04-08 13:32:20 +0200

    Our crude, very basic HTTP server with "routes".

--- 01-intro/http.js
+++ 01-intro/http.js
@@ -4,30 +4,44 @@
 const fs = require('fs');
 const http = require('http');
 
+const urls = [
+	{
+		url: '/',
+		file: './pages/index.html',
+		type: 'text/html',
+	},
+	{
+		url: '/about',
+		file: './pages/about.html',
+		type: 'text/html',
+	},
+	{
+		url: '/memes',
+		file: './pages/memes.html',
+		type: 'text/html',
+	},
+	{
+		url: '/style.css',
+		file: './pages/style.css',
+		type: 'text/css',
+	},
+];
+
 const server = http.createServer();
 server.on('request', function (req, res) {
 	console.log(`Incoming ${req.method} request for: ${req.url}`);
 
+	const route = urls.find(route => route.url === req.url);
+	if (!route) {
+		res.writeHead(404);
+		res.end();
+		return;
+	}
+
 	res.writeHead(200, {
-		'Content-Type': 'text/plain',
-		'Content-Encoding': 'utf-8',
+		'Content-Type': route.type,
 	});
-
-	const readStream = fs.createReadStream('./data/oneliners.txt', 'utf-8');
-	// readStream.on('data', chunk => {
-	// 	res.write(chunk);
-	// });
-
-	// readStream.on('end', () => {
-	// 	res.end();
-	// });
-
-	readStream.pipe(res);
-
-	// res.write('Hello, visitor!');
-	// res.write(`You have requested ${req.url} using ${req.method} and it's ${Date()} right now.`);
-	// res.write(`Byyyye!`);
-	// res.end();
+	fs.createReadStream(route.file, 'utf-8').pipe(res);
 });
 
 server.listen(8080);
