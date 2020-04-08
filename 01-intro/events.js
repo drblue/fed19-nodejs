da6960219a0b8d901c92c58f601547584f9873e1
Johan Nordström <johan@digitalvillage.se>
2020-04-08 09:31:46 +0200

    Events with multiple parameters.

--- 01-intro/events.js
+++ 01-intro/events.js
@@ -17,13 +17,13 @@ let tom = new Person('Tom'); // https://www.youtube.com/channel/UCBa659QWEk1AI4T
 let people = [derek, linus, simone, tom];
 
 people.forEach(person => {
-	person.on('speak', msg => {
-		console.log(`${person.name} said: ${msg}\n`);
+	person.on('speak', (msg, scream = false) => {
+		console.log(`${person.name} said: ${scream ? msg.toUpperCase() : msg}\n`);
 	});
 });
 
 derek.emit('speak', 'In Quantum Mechanics, if you know the Quantum State of a particle, that is it\'s wave function you can use the Schrodinger equation to calculate what that particle will do in the future.');
 
-linus.emit('speak', 'But it has basically zero competition in this price bracket, so we absolutely needed to take a look at it. Just like I need to tell you about our sponsor - Storyblocks!');
+linus.emit('speak', 'But it has basically zero competition in this price bracket, so we absolutely needed to take a look at it. Just like I need to tell you about our sponsor - Storyblocks!', true);
 
 simone.emit('speak', 'My track record isn\'t terribly impressive so far, but I have an angle grinder, and a welder, and I\'m not afraid to use them.');
