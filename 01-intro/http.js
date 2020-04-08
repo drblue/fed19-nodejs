060f32085ca979ab7887fefcb3ffd4e677639f2b
Johan Nordström <johan@digitalvillage.se>
2020-04-08 11:21:27 +0200

    Streaming HTTP server.

--- 01-intro/http.js
+++ 01-intro/http.js
@@ -1,17 +1,33 @@
 /**
  * http
  */
+const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer();
 server.on('request', function (req, res) {
-	console.log("Hey, someone is requesting something from us! Yayyyy!");
+	console.log(`Incoming ${req.method} request for: ${req.url}`);
 
 	res.writeHead(200, {
-		'Content-Type': 'text/html',
+		'Content-Type': 'text/plain',
+		'Content-Encoding': 'utf-8',
 	});
 
-	res.end(`Hello, browser! It's ${Date()} right now.`);
+	const readStream = fs.createReadStream('./data/oneliners.txt', 'utf-8');
+	// readStream.on('data', chunk => {
+	// 	res.write(chunk);
+	// });
+
+	// readStream.on('end', () => {
+	// 	res.end();
+	// });
+
+	readStream.pipe(res);
+
+	// res.write('Hello, visitor!');
+	// res.write(`You have requested ${req.url} using ${req.method} and it's ${Date()} right now.`);
+	// res.write(`Byyyye!`);
+	// res.end();
 });
 
 server.listen(8080);
