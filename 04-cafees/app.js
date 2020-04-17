d43005bfe91981772739329a687a55fb42e9ecc3
Johan Nordström <johan@digitalvillage.se>
2020-04-17 09:24:00 +0200

    Using default values in config.

--- 04-cafees/app.js
+++ 04-cafees/app.js
@@ -27,11 +27,11 @@ app.use((req, res, next) => {
 const getDbConnection = () => {
 	// connect to database
 	const db = mysql.createConnection({
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		host: process.env.DB_HOST || 'localhost',
+		port: process.env.DB_PORT || 3306,
+		user: process.env.DB_USER || 'fika',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'fika',
 	});
 	db.connect();
 	return db;
