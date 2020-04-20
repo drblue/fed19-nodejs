68a7a9c04acb7cbb55e000c8b759038986e4e8f0
Johan Nordström <johan@digitalvillage.se>
2020-04-20 11:04:35 +0200

    Moved cafee routes and cafee logic to a separate route-module.

--- 04-cafees/app.js
+++ 04-cafees/app.js
@@ -9,7 +9,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const moment = require('moment');
 
-const cafeeDb = require('./db/cafee');
+const cafeeRouter = require('./routes/cafee');
 
 // set ejs as our template engine
 app.set('view engine', 'ejs');
@@ -25,121 +25,7 @@ app.use((req, res, next) => {
 	next();
 });
 
-// show all cafées
-app.get('/cafees', (req, res) => {
-	// ask database nicely for a list of all cafés
-	cafeeDb.getAll()
-	.then(cafees => {
-		// once we get the list, send it to the view
-		res.render('cafees/index', {
-			cafees,
-		});
-	})
-	.catch(error => {
-		res.status(500).send('Sorry, database threw an error when trying to get all cafees.');
-		throw error;
-	});
-});
-
-// create new cafe form
-app.get('/cafees/create', (req, res) => {
-	res.render('cafees/create');
-});
-
-// create new cafe in db
-app.post('/cafees', (req, res) => {
-	console.log("Would like to create a new cafee...");
-
-	const cafee = {
-		name: req.body.name,
-		address: req.body.address,
-		city: req.body.city,
-	}
-
-	cafeeDb.store(cafee)
-	.then(results => {
-		console.log("Created a new café with ID", results[0]);
-		res.redirect('/cafees/' + results[0]);
-	})
-	.catch(error => {
-		res.status(500).send('Sorry, could not create a new café.');
-		throw error;
-	});
-});
-
-// show specific café
-app.get('/cafees/:cafeId', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	cafeeDb.get(cafeId)
-	.then(cafee => {
-		// once we get the café, send it to the view
-		res.render('cafees/show', {
-			cafee,
-		});
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
-		throw error;
-	});
-});
-
-// show edit café form
-app.get('/cafees/:cafeId/edit', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	cafeeDb.get(cafeId)
-	.then(cafee => {
-		// once we get the café, send it to the view
-		res.render('cafees/edit', {
-			cafee,
-		});
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
-		throw error;
-	});
-});
-
-// update café with form data
-app.post('/cafees/:cafeId', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	console.log(`Would like to update cafee with ID ${cafeId}...`);
-
-	const cafee = {
-		name: req.body.name || 'Default Café Name',
-		address: req.body.address,
-		city: req.body.city,
-	}
-
-	cafeeDb.update(cafeId, cafee)
-	.then(() => {
-		console.log(`Updated café with ID ${cafeId}`);
-		res.redirect('/cafees/' + cafeId);
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, could not update café with ID ${cafeId}.`);
-		throw error;
-	});
-});
-
-// delete a café from db
-app.post('/cafees/:cafeId/delete', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	console.log(`Want to delete café with ID ${cafeId}...`);
-
-	cafeeDb.destroy(cafeId)
-	.then(() => {
-		console.log(`Deleted café with ID ${cafeId}`);
-		res.redirect('/cafees');
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, could not delete café with ID ${cafeId}.`);
-		throw error;
-	})
-});
+app.use('/cafees', cafeeRouter);
 
 // serve static files from `/public` folder
 // using the express static middleware
