482bddb5c95ecda20f5fd5666faf3aef3f448c12
Johan Nordström <johan@digitalvillage.se>
2020-04-20 12:07:42 +0200

    Moved all cafee logic to its own Controller module.

---
+++ 04-cafees/controllers/cafee.js
@@ -0,0 +1,124 @@
+/**
+ * Cafee controller
+ */
+
+const cafeeDb = require('../db/cafee');
+
+const index = (req, res) => {
+	// ask database nicely for a list of all cafés
+	cafeeDb.getAll()
+	.then(cafees => {
+		// once we get the list, send it to the view
+		res.render('cafees/index', {
+			cafees,
+		});
+	})
+	.catch(error => {
+		res.status(500).send('Sorry, database threw an error when trying to get all cafees.');
+		throw error;
+	});
+};
+
+const create = (req, res) => {
+	res.render('cafees/create');
+};
+
+const store = (req, res) => {
+	console.log("Would like to create a new cafee...");
+
+	const cafee = {
+		name: req.body.name,
+		address: req.body.address,
+		city: req.body.city,
+	}
+
+	cafeeDb.store(cafee)
+	.then(results => {
+		console.log("Created a new café with ID", results[0]);
+		res.redirect('/cafees/' + results[0]);
+	})
+	.catch(error => {
+		res.status(500).send('Sorry, could not create a new café.');
+		throw error;
+	});
+};
+
+const show = (req, res) => {
+	const cafeId = req.params.cafeId;
+
+	cafeeDb.get(cafeId)
+	.then(cafee => {
+		// once we get the café, send it to the view
+		res.render('cafees/show', {
+			cafee,
+		});
+	})
+	.catch(error => {
+		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
+		throw error;
+	});
+};
+
+const edit = (req, res) => {
+	const cafeId = req.params.cafeId;
+
+	cafeeDb.get(cafeId)
+	.then(cafee => {
+		// once we get the café, send it to the view
+		res.render('cafees/edit', {
+			cafee,
+		});
+	})
+	.catch(error => {
+		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
+		throw error;
+	});
+};
+
+const update = (req, res) => {
+	const cafeId = req.params.cafeId;
+
+	console.log(`Would like to update cafee with ID ${cafeId}...`);
+
+	const cafee = {
+		name: req.body.name || 'Default Café Name',
+		address: req.body.address,
+		city: req.body.city,
+	}
+
+	cafeeDb.update(cafeId, cafee)
+	.then(() => {
+		console.log(`Updated café with ID ${cafeId}`);
+		res.redirect('/cafees/' + cafeId);
+	})
+	.catch(error => {
+		res.status(500).send(`Sorry, could not update café with ID ${cafeId}.`);
+		throw error;
+	});
+};
+
+const destroy = (req, res) => {
+	const cafeId = req.params.cafeId;
+
+	console.log(`Want to delete café with ID ${cafeId}...`);
+
+	cafeeDb.destroy(cafeId)
+	.then(() => {
+		console.log(`Deleted café with ID ${cafeId}`);
+		res.redirect('/cafees');
+	})
+	.catch(error => {
+		res.status(500).send(`Sorry, could not delete café with ID ${cafeId}.`);
+		throw error;
+	})
+};
+
+module.exports = {
+	index,
+	create,
+	store,
+	show,
+	edit,
+	update,
+	destroy,
+}
--- 04-cafees/routes/cafee.js
+++ 04-cafees/routes/cafee.js
@@ -4,122 +4,27 @@
 
 const express = require('express');
 const router = express.Router();
-const cafeeDb = require('../db/cafee');
+const { index, create, store, show, edit, update, destroy } = require('../controllers/cafee');
 
 // show all cafées
-router.get('/', (req, res) => {
-	// ask database nicely for a list of all cafés
-	cafeeDb.getAll()
-	.then(cafees => {
-		// once we get the list, send it to the view
-		res.render('cafees/index', {
-			cafees,
-		});
-	})
-	.catch(error => {
-		res.status(500).send('Sorry, database threw an error when trying to get all cafees.');
-		throw error;
-	});
-});
+router.get('/', index);
 
 // create new cafe form
-router.get('/create', (req, res) => {
-	res.render('cafees/create');
-});
+router.get('/create', create);
 
 // create new cafe in db
-router.post('/', (req, res) => {
-	console.log("Would like to create a new cafee...");
-
-	const cafee = {
-		name: req.body.name,
-		address: req.body.address,
-		city: req.body.city,
-	}
-
-	cafeeDb.store(cafee)
-	.then(results => {
-		console.log("Created a new café with ID", results[0]);
-		res.redirect('/cafees/' + results[0]);
-	})
-	.catch(error => {
-		res.status(500).send('Sorry, could not create a new café.');
-		throw error;
-	});
-});
+router.post('/', store);
 
 // show specific café
-router.get('/:cafeId', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	cafeeDb.get(cafeId)
-	.then(cafee => {
-		// once we get the café, send it to the view
-		res.render('cafees/show', {
-			cafee,
-		});
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
-		throw error;
-	});
-});
+router.get('/:cafeId', show);
 
 // show edit café form
-router.get('/:cafeId/edit', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	cafeeDb.get(cafeId)
-	.then(cafee => {
-		// once we get the café, send it to the view
-		res.render('cafees/edit', {
-			cafee,
-		});
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`);
-		throw error;
-	});
-});
+router.get('/:cafeId/edit', edit);
 
 // update café with form data
-router.post('/:cafeId', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	console.log(`Would like to update cafee with ID ${cafeId}...`);
-
-	const cafee = {
-		name: req.body.name || 'Default Café Name',
-		address: req.body.address,
-		city: req.body.city,
-	}
-
-	cafeeDb.update(cafeId, cafee)
-	.then(() => {
-		console.log(`Updated café with ID ${cafeId}`);
-		res.redirect('/cafees/' + cafeId);
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, could not update café with ID ${cafeId}.`);
-		throw error;
-	});
-});
+router.post('/:cafeId', update);
 
 // delete a café from db
-router.post('/:cafeId/delete', (req, res) => {
-	const cafeId = req.params.cafeId;
-
-	console.log(`Want to delete café with ID ${cafeId}...`);
-
-	cafeeDb.destroy(cafeId)
-	.then(() => {
-		console.log(`Deleted café with ID ${cafeId}`);
-		res.redirect('/cafees');
-	})
-	.catch(error => {
-		res.status(500).send(`Sorry, could not delete café with ID ${cafeId}.`);
-		throw error;
-	})
-});
+router.post('/:cafeId/delete', destroy);
 
 module.exports = router;
