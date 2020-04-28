8cb7cfd60d112e3c734893360f51f20af5fbe145
Johan Nordström <johan@digitalvillage.se>
2020-04-28 14:07:31 +0200

    Cafee API index and show endpoints.

--- 04-cafees/controllers/api/api_cafee_controller.js
+++ 04-cafees/controllers/api/api_cafee_controller.js
@@ -2,16 +2,22 @@
  * API Café Controller
  */
 
+const cafees = require('../../db/cafees_db');
+const categories = require('../../db/categories_db');
+const owners = require('../../db/owners_db');
+
 // Get index of all cafés
 const index = (req, res) => {
-	res.send([
-		{
-			name: 'Gustavs Go-fika',
-		},
-		{
-			name: 'Fredriks Ful-fika',
-		},
-	]);
+	cafees.getAll()
+	.then(cafees => {
+		res.send(cafees);
+	})
+	.catch(error => {
+		res.status(500).send({
+			error: 'Sorry, database threw an error when trying to get all cafees.',
+		});
+		throw error;
+	});
 }
 
 // Create a café
@@ -20,8 +26,32 @@ const store = (req, res) => {
 };
 
 // Get a specific café
-const show = (req, res) => {
-	res.send({ message: 'GET /' + req.params.cafeId });
+const show = async (req, res) => {
+	const cafeId = req.params.cafeId;
+
+	try {
+		const cafee = await cafees.get(cafeId);
+		if (!cafee) {
+			res.status(404).send({
+				error: `Sorry, cafee with ID ${cafeId} could not be found.`,
+			});
+			return;
+		}
+
+		cafee.owner = (cafee.owner_id)
+			? await owners.get(cafee.owner_id)
+			: false;
+
+		cafee.categories = await categories.getForCafee(cafeId);
+
+		res.send(cafee);
+
+	} catch (error) {
+		res.status(500).send({
+			error: `Sorry, database threw an error when trying to get cafee with ID ${cafeId}.`,
+		});
+		throw error;
+	}
 };
 
 // Update a specific café
