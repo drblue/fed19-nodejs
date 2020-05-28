077ad49bfa4b286ea3e0c8e7b54ec244eb1748a7
Johan Nordström <johan@digitalvillage.se>
2020-05-28 11:11:57 +0200

    Added mongoose and established connection to MongoDB Atlas instance.

---
+++ 08-lmdb/models/index.js
@@ -0,0 +1,25 @@
+/**
+ * Models
+ */
+
+const mongoose = require('mongoose');
+
+// Connect to database
+mongoose.connect(process.env.DB_CONNECTION, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	// we're connected!
+	console.log("We're connected to MongoDB Atlas! 🥳");
+});
+
+// Set up the models we want to use in our app
+
+// Export all the things
+module.exports = {
+	mongoose,
+}
