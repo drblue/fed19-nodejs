03119f8553bc0ee30d8ae8a68dfd39e889af51b1
Johan Nordström <johan@digitalvillage.se>
2020-06-09 14:43:54 +0200

    Fixed "this" 😆.

--- 09-chat/controllers/socket_controller.js
+++ 09-chat/controllers/socket_controller.js
@@ -5,34 +5,49 @@
 const debug = require('debug')('09-simple-chat:socket_controller');
 const users = {};
 
-module.exports = (socket) => {
-	debug("A client connected!");
+/**
+ * Handle user disconnecting
+ */
+function handleUserDisconnect() {
+	debug(`Socket ${this.id} left the chat :(`);
 
-	socket.on('disconnect', () => {
-		debug(`Socket ${socket.id} left the chat :(`);
+	// broadcast to all connected sockets that this user has left the chat
+	if (users[this.id]) {
+		this.broadcast.emit('user-disconnected', users[this.id]);
+	}
 
-		// broadcast to all connected sockets that this user has left the chat
-		if (users[socket.id]) {
-			socket.broadcast.emit('user-disconnected', users[socket.id]);
-		}
+	// remove user from list of connected users
+	delete users[this.id];
+}
 
-		// remove user from list of connected users
-		delete users[socket.id];
-	});
+/**
+ * Handle incoming chat-message
+ */
+function handleChatMsg (msg) {
+	debug("Someone sent something nice: '%s'", msg);
+	//io.emit('chatmsg', msg); // emit to all connected sockets
 
-	socket.on('register-user', username => {
-		debug("User '%s' connected to the chat", username);
-		users[socket.id] = username;
+	// broadcast to all connected sockets EXCEPT ourselves
+	this.broadcast.emit('chatmsg', msg);
+}
 
-		// broadcast to all connected sockets EXCEPT ourselves
-		socket.broadcast.emit('new-user-connected', username);
-	});
+/**
+ * Handle a new user connecting
+ */
+function handleRegisterUser(username) {
+	debug("User '%s' connected to the chat", username);
+	users[this.id] = username;
 
-	socket.on('chatmsg', (msg) => {
-		debug("Someone sent something nice: '%s'", msg);
-		//io.emit('chatmsg', msg); // emit to all connected sockets
+	// broadcast to all connected sockets EXCEPT ourselves
+	this.broadcast.emit('new-user-connected', username);
+}
 
-		// broadcast to all connected sockets EXCEPT ourselves
-		socket.broadcast.emit('chatmsg', msg);
-	});
+module.exports = function(socket) {
+	// this = io
+	debug(`Client ${socket.id} connected!`);
+
+	socket.on('disconnect', handleUserDisconnect);
+
+	socket.on('chatmsg', handleChatMsg);
+	socket.on('register-user', handleRegisterUser);
 }
