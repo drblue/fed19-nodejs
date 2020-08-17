61c6bf3ff8a66d28435f0d0afb0c9914ff1e5110
Johan Nordström <johan@digitalvillage.se>
2020-08-17 10:27:00 +0200

    Added node-sass, bootstrap and socket.io-client.

--- 11-need-halp-plz/client/src/App.js
+++ 11-need-halp-plz/client/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import './App.scss';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				Halp!
-			</header>
-			<h1>Plz log in </h1>
+		<div id="App">
+			<nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+				<div className="container">
+					<a className="navbar-brand" href="#">Halp! 😨</a>
+						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+
+					<div className="collapse navbar-collapse" id="navbarMain">
+						<ul className="navbar-nav ml-auto">
+							<li className="nav-item active">
+								<a className="nav-link" href="#">Register</a>
+							</li>
+							<li className="nav-item active">
+								<a className="nav-link" href="#">Login</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<main role="main" className="container">
+
+				<div className="">
+					<h1>Need Halp Plz</h1>
+					<p className="lead">Please log in.</p>
+				</div>
+
+			</main>
 		</div>
 	);
 }
