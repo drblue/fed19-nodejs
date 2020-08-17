2aa0856c034140f88380e2827e1240a13cacd963
Johan Nordström <johan@digitalvillage.se>
2020-08-17 10:35:44 +0200

    Moved navigation to a separate component and added react-router-dom pcakge.

---
+++ 11-need-halp-plz/client/src/components/navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navigation = props => {
+	return (
+		<nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+			<div className="container">
+				<a className="navbar-brand" href="#">Halp! 😨</a>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+
+				<div className="collapse navbar-collapse" id="navbarMain">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">Register</a>
+						</li>
+						<li className="nav-item active">
+							<a className="nav-link" href="#">Login</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+export default Navigation;
