2484f58d7ad131bfc5f4885ef9f68562779ca656
Johan Nordström <johan@digitalvillage.se>
2020-08-17 10:59:30 +0200

    Wrapped Navigation-component with react-router-dom.

--- 11-need-halp-plz/client/src/components/navigation/Navigation.js
+++ 11-need-halp-plz/client/src/components/navigation/Navigation.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
 const Navigation = props => {
 	return (
 		<nav className="navbar navbar-expand-md navbar-dark bg-dark">
 			<div className="container">
-				<a className="navbar-brand" href="#">Halp! 😨</a>
-					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
+				<Link to='/' className="navbar-brand">I need help!</Link>
+
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
 
 				<div className="collapse navbar-collapse" id="navbarMain">
 					<ul className="navbar-nav ml-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="#">Register</a>
+						<li className="nav-item">
+							<NavLink exact to="/" className="nav-link">Login</NavLink>
 						</li>
-						<li className="nav-item active">
-							<a className="nav-link" href="#">Login</a>
+						<li className="nav-item">
+							<NavLink to="/room/fed19" className="nav-link">Room: FED19</NavLink>
 						</li>
 					</ul>
 				</div>
@@ -24,4 +26,4 @@ const Navigation = props => {
 	)
 }
 
-export default Navigation;
+export default withRouter(Navigation);
