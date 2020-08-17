27f5980f90d2a58a7765fd7f7b85b13c4967552f
Johan Nordström <johan@digitalvillage.se>
2020-08-17 09:49:17 +0200

    Added React boilerplate and nodemon config.

---
+++ 11-need-halp-plz/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				Halp!
+			</header>
+			<h1>Plz log in </h1>
+		</div>
+	);
+}
+
+export default App;
