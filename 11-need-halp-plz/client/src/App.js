c6d20ccfd3e81c2b971f627bfca27f56521dce3f
Johan Nordström <johan@digitalvillage.se>
2020-08-18 14:22:49 +0200

    Added Axios and /room route for listing all rooms in frontend.

--- 11-need-halp-plz/client/src/App.js
+++ 11-need-halp-plz/client/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/login/Login';
 import Navigation from './components/navigation/Navigation';
 import NotFound from './components/notfound/NotFound';
 import Room from './components/room/Room';
+import RoomList from './components/room/RoomList';
 import socket from './modules/socket-client';
 import './App.scss';
 
@@ -27,7 +28,10 @@ class App extends React.Component {
 					<main role="main" className="container my-3">
 						<Switch>
 							<Route exact path='/' component={Login} />
+
+							<Route path='/room' component={RoomList} />
 							<Route path='/room/:id' component={Room} />
+
 							<Route component={NotFound} />
 						</Switch>
 					</main>
---
+++ 11-need-halp-plz/client/src/components/room/RoomList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class RoomList extends React.Component {
+
+	state = {
+		loading: true,
+		roomlist: [],
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:3001/rooms')
+		.then(res => {
+			console.log("Got response!", res);
+			if (res.data.status === "success") {
+				const rooms = res.data.data.rooms;
+				this.setState({
+					loading: false,
+					roomlist: rooms,
+				});
+			}
+		})
+	}
+
+	componentWillUnmount() {
+	}
+
+	render() {
+		return (
+			<div className="component-Roomlist">
+				{this.state.loading ? (
+					<h1>Loading...</h1>
+				) : (
+					<>
+						<div className="text-center">
+							<h1>Room List</h1>
+							<p className="lead">Choose which school-class you belong to</p>
+						</div>
+
+						<ol className="list-group">
+							{
+								this.state.roomlist.map((room, index) => (
+									<li className="list-group-item" key={index}>
+										<Link to={'/room/' + room.name}>
+											{room.name}
+										</Link>
+									</li>
+								))
+							}
+						</ol>
+					</>
+				)}
+			</div>
+		)
+	}
+}
+
+export default RoomList;
